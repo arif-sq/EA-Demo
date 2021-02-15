d1cdb72ec7d270fd23ceefd4fe9ab4f904515e8d
arif-sq <arifur@outlook.com>
2021-02-15 17:51:31 +0600

    Hmm

--- src/main/java/com/wpdev/ea/utils/config.java
+++ src/main/java/com/wpdev/ea/utils/config.java
@@ -7,7 +7,8 @@ public class config {
 		public static final String base_url= "https://essential-addons.com/elementor";
 		public static final String nx_close = "//*[@id=\"notificationx-600926476622b\"]/div/span/svg";
 		public static final String crisp_close = "//*[@id=\"crisp-chatbox\"]/div/a/span[1]/span/span[1]/span[1]/span";
-		public static final String doc_check = "YES";
+		public static final String doc_check = "NO";
+		//public static final String doc_check = "YES";
 		
 		
 
